b9ca81bf638349b522f039408ef5ab9dbfcc56c5
cgkim449 <cgkim449@gmail.com>
2020-10-24 21:20:13 +0900

    test

--- pokemon-golder-server/src/main/java/pokemon/golder/server/pms/handler/HelloCommand.java
+++ pokemon-golder-server/src/main/java/pokemon/golder/server/pms/handler/HelloCommand.java
@@ -3,7 +3,6 @@ package pokemon.golder.server.pms.handler;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 
-// Command 규칙에 따라 클래스를 정의한다.
 public class HelloCommand implements Command {
 
   @Override
